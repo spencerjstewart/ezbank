a7f7a5539605d37b632fc96768bce4861ffb4356
Spencer Stewart <spencerjamesstewart@gmail.com>
2024-09-23 14:00:05 -0500

    create Account and Customer repository

---
+++ accounts/src/main/java/com/spencerstewart/ezbank/accounts/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.spencerstewart.ezbank.accounts.repository;
+
+import com.spencerstewart.ezbank.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+
+}
