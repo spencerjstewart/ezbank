0df10636157877798556352789ebae9c9b78cd60
Spencer Stewart <spencerjamesstewart@gmail.com>
2024-09-23 21:35:42 -0500

    create a global exception handler

--- accounts/src/main/java/com/spencerstewart/ezbank/accounts/controller/AccountController.java
+++ accounts/src/main/java/com/spencerstewart/ezbank/accounts/controller/AccountController.java
@@ -4,6 +4,8 @@ import com.spencerstewart.ezbank.accounts.constants.AccountConstant;
 import com.spencerstewart.ezbank.accounts.dto.CustomerDto;
 import com.spencerstewart.ezbank.accounts.dto.ResponseDto;
 
+import com.spencerstewart.ezbank.accounts.service.AccountService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,10 +13,15 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
+@AllArgsConstructor
 public class AccountController {
+
+	private AccountService accountService;
 
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+
+		accountService.createAccount(customerDto);
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new ResponseDto(AccountConstant.STATUS_201, AccountConstant.MESSAGE_201));
